2d174507a68e988dd59797d1fc23b22f05df1c26
GBN <58118380+GraczBezNicku@users.noreply.github.com>
2023-01-18 13:46:57 +0100

    Add files via upload

---
+++ EventsHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MEC;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+
+namespace TemporaryGruopAssignment
+{
+    public class EventsHandler
+    {
+        [PluginEvent(PluginAPI.Enums.ServerEventType.PlayerJoined)]
+        public void OnPlayerVerified(Player player)
+        {
+            Timing.CallDelayed(0.5f, () => API.CheckForExpiration(player));
+        }
+    }
+}
