0e0c14923f573111e0f84b55c94ba5b7f779a213
GBN <58118380+GraczBezNicku@users.noreply.github.com>
2023-07-27 20:27:18 +0200

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemporaryGruopAssignment
+{
+    public class Config
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        [Description("How often should the plugin check if player's group expired (in seconds)")]
+        public float CheckInterval { get; set; } = 30;
+    }
+}
